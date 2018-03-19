bc360f8ea25fb5fd74373df4bc31d5477f7a023a
Lukegits <luket.chavez@gmail.com>
2018-03-19 14:43:51 -0600

    audio play back checkpoint work

--- src/components/Album.js
+++ src/components/Album.js
@@ -12,10 +12,37 @@ class Album extends Component {
    console.log(album);
 
    this.state = {
-     album: album
+     album: album,
+      currentSong: album.songs[0],
+      isPlaying: false
    };
 
+  this.audioElement = document.createElement('audio');
+  this.audioElement.src = album.songs[0].audioSrc;
+
    }
+  play() {
+    this.audioElement.play();
+    this.setState({ isPlaying: true });
+  }
+  pause() {
+     this.audioElement.pause();
+     this.setState({ isPlaying: false });
+   }
+   setSong(song) {
+     this.audioElement.src = song.audioSrc;
+     this.setState({ currentSong: song });
+   }
+   handleSongClick(song) {
+     const isSameSong = this.state.currentSong === song;
+     if (this.state.isPlaying && isSameSong) {
+     this.pause();
+   } else {
+     if (!isSameSong) { this.setSong(song); }
+     this.play();
+   }
+   }
+
    render() {
      return (
        <section className="album">
@@ -37,12 +64,14 @@ class Album extends Component {
           <section className='songs-titles'>
           {
            this.state.album.songs.map( (song, index) =>
-             <div>
-                 <tr> <i class="icon ion-play"> </i> <i class="icon ion-pause"> </i>
+           <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
+
+                 <td className="song actions"> <i class="icon ion-play"> </i> <i class="icon ion-pause"> </i>
                    {index + 1}
                    {song.title} {song.duration}
-                   </tr>
-             </div>
+                  </td>
+             </tr>
+
           )
         }
            </section>
