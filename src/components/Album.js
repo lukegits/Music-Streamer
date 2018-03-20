71c64360f824866abadb495725554792b0319e2b
Lukegits <luket.chavez@gmail.com>
2018-03-20 14:14:40 -0600

    buttons assignment work

--- src/components/Album.js
+++ src/components/Album.js
@@ -44,11 +44,18 @@ class Album extends Component {
    }
    }
    handlePrevClick() {
-     const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+      const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
       const newIndex = Math.max(0, currentIndex - 1);
       const newSong = this.state.album.songs[newIndex];
       this.setSong(newSong);
       this.play(newSong);
+   }
+   handleNextClick() {
+     const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+     const newIndex = Math.max(0, currentIndex + 1);
+     const newSong = this.state.album.songs[newIndex];
+     this.setSong(newSong);
+     this.play(newSong);
    }
 
    render() {
@@ -90,6 +97,7 @@ class Album extends Component {
           currentSong={this.state.currentSong}
           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
           handlePrevClick={() => this.handlePrevClick()}
+          handleNextClick={() => this.handleNextClick()}
         />
       </section>
      )
